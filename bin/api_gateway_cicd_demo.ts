29d5d6c88221bad8df9eeea2913dd362e6b329fb
sadiqchigur <sadiq.chigur0391@gmail.com>
2025-12-16 18:50:05 +0530

    Rename ApiGatewayStack to ApiGatewayCicdDemoStack

--- bin/api_gateway_cicd_demo.ts
+++ bin/api_gateway_cicd_demo.ts
@@ -103,7 +103,7 @@ new ApiGatewayCicdDemoStack(app, 'ApiGwSharedStack', {
 #!/usr/bin/env node
 import 'source-map-support/register';
 import * as cdk from 'aws-cdk-lib';
-import { ApiGatewayStack } from '../lib/api-gateway-stack';
+import { ApiGatewayCicdDemoStack } from '../lib/api_gateway_cicd_demo-stack';
 
 const app = new cdk.App();
 
